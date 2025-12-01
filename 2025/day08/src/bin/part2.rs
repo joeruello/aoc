330a6d3737bd955ca9061fe2e0a2444a7dde581e
Joe Ruello <joeruello@gmail.com>
2025-12-01 16:37:45 +1100

    add in templates for rest of the year

---
+++ 2025/day08/src/bin/part2.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let input: String = common::AocInput::fetch(2025, 8).unwrap().into();
+    println!("Output: {}", process(&input));
+}
+
+fn process(input: &str) -> i32 {
+    todo!();
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_processes() {
+//         assert_eq!(process(include_str!("./sample.txt")), 3);
+//     }
+// }
