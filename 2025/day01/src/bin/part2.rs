9e6bbb7c57e165c30b730b251a1163272b4a535d
Joe Ruello <joeruello@gmail.com>
2025-12-01 16:33:19 +1100

    2025-day01

---
+++ 2025/day01/src/bin/part2.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input: String = common::AocInput::fetch(2025, 1).unwrap().into();
+    println!("Output: {}", process(&input));
+}
+
+fn process(input: &str) -> i32 {
+    let rots: Vec<_> = input
+        .lines()
+        .map(|l| {
+            let (dir, num) = l.split_at(1);
+            let dir = match &dir.chars().next().unwrap() {
+                'R' => 1,
+                'L' => -1,
+                _ => {
+                    panic!("unknown direction {dir}")
+                }
+            };
+            let num = num.parse::<i32>().unwrap();
+            dir * num
+        })
+        .collect();
+
+    let mut crossed = 0;
+    let mut sum = 50;
+
+    for n in rots {
+        sum += n;
+        if !(0..=99).contains(&sum) {
+            crossed += sum.div_euclid(100).abs();
+            sum = sum.rem_euclid(100);
+        };
+    }
+
+    crossed
+}
