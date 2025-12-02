ee4d143c8a661a344e3846e48d636fc9dd4c3395
Joe Ruello <joeruello@gmail.com>
2025-12-02 18:12:30 +1100

    2025 day02

--- 2025/day02/src/bin/part1.rs
+++ 2025/day02/src/bin/part1.rs
@@ -1,18 +1,59 @@
+use common::Itertools;
+
 fn main() {
     let input: String = common::AocInput::fetch(2025, 2).unwrap().into();
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+fn process(input: &str) -> u64 {
+    let ids = input
+        .strip_suffix('\n')
+        .unwrap()
+        .split(',')
+        .flat_map(|l| {
+            dbg!(l);
+            let (a, b) = l.split_once('-').unwrap();
+
+            [a.parse::<u64>().unwrap()..=b.parse::<u64>().unwrap()]
+        })
+        .collect_vec();
+    let mut sum = 0;
+    for range in ids {
+        for id in range {
+            if !is_valid(id) {
+                sum += id;
+            }
+        }
+    }
+
+    sum
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+fn is_valid(id: u64) -> bool {
+    let id = format!("{id}");
+    if id.len() % 2 == 1 {
+        return true;
+    }
+    let idx = id.len() / 2;
+    let start = &id[..idx];
+    let end = &id[idx..];
+    !start.eq(end)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process(include_str!("./sample.txt")), 1227775554);
+    }
+
+    #[test]
+    fn it_validates() {
+        assert!(is_valid(1));
+        assert!(!is_valid(11));
+        assert!(!is_valid(123123));
+        assert!(!is_valid(1188511885))
+    }
+}
--- 2025/day02/src/bin/part2.rs
+++ 2025/day02/src/bin/part2.rs
@@ -1,18 +1,62 @@
+use common::Itertools;
+
 fn main() {
     let input: String = common::AocInput::fetch(2025, 2).unwrap().into();
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+fn process(input: &str) -> u64 {
+    let ids = input
+        .strip_suffix('\n')
+        .unwrap()
+        .split(',')
+        .flat_map(|l| {
+            dbg!(l);
+            let (a, b) = l.split_once('-').unwrap();
+
+            [a.parse::<u64>().unwrap()..=b.parse::<u64>().unwrap()]
+        })
+        .collect_vec();
+    let mut sum = 0;
+    for range in ids {
+        for id in range {
+            if !is_valid(id) {
+                sum += id;
+            }
+        }
+    }
+
+    sum
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+fn is_valid(id: u64) -> bool {
+    let s = format!("{id}");
+    for i in 1..s.len() {
+        if s == s[..i].repeat(s.len() / i) {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process(include_str!("./sample.txt")), 4174379265);
+    }
+
+    #[test]
+    fn it_validates() {
+        assert!(is_valid(1698522));
+        assert!(!is_valid(11));
+        assert!(!is_valid(123123));
+        assert!(!is_valid(1188511885));
+        assert!(!is_valid(824824824));
+        assert!(is_valid(824824823));
+        assert!(!is_valid(565656))
+    }
+}
