649e36a64e28dd921c1cd26254f71a3c73457098
Joe Ruello <joeruello@gmail.com>
2025-12-03 17:16:23 +1100

    day 3 2025

--- 2025/day03/src/bin/part1.rs
+++ 2025/day03/src/bin/part1.rs
@@ -3,16 +3,35 @@ fn main() {
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+fn process(input: &str) -> u32 {
+    let batteries: Vec<Vec<u32>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+
+    let mut sum = 0;
+    for bat in batteries {
+        let (i, tens) = &bat[..bat.len() - 1]
+            .iter()
+            .enumerate()
+            .rev()
+            .max_by_key(|(_, b)| *b)
+            .unwrap();
+        dbg!(&bat, &tens);
+        let ones = dbg!(&bat[*i + 1..bat.len()]).iter().max().unwrap();
+
+        sum += dbg!((**tens * 10) + *ones)
+    }
+
+    sum
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process(include_str!("./sample.txt")), 358);
+    }
+}
--- 2025/day03/src/bin/part2.rs
+++ 2025/day03/src/bin/part2.rs
@@ -3,16 +3,41 @@ fn main() {
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+fn process(input: &str) -> u64 {
+    let batteries: Vec<Vec<u64>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap().into()).collect())
+        .collect();
+
+    let mut sum = 0;
+    for bat in batteries {
+        let mut s: u64 = 0;
+        let mut i = 0;
+        for place in (0..12).rev() {
+            let (j, num) = &bat[i..bat.len() - place]
+                .iter()
+                .enumerate()
+                .rev()
+                .max_by_key(|(_, b)| *b)
+                .unwrap()
+                .to_owned();
+
+            i += *j + 1;
+            s += **num * 10u64.pow(place as u32)
+        }
+        sum += s;
+    }
+
+    sum
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process("987654321111111"), 987654321111);
+        assert_eq!(process(include_str!("./sample.txt")), 3121910778619);
+    }
+}
