d32ca1f59af7fc76d420e000d9c1999552f9ca74
Joe Ruello <joeruello@gmail.com>
2025-12-07 18:16:29 +1100

    2025 day06

--- 2025/day06/src/bin/part1.rs
+++ 2025/day06/src/bin/part1.rs
@@ -1,18 +1,46 @@
+use std::collections::HashMap;
+
 fn main() {
     let input: String = common::AocInput::fetch(2025, 6).unwrap().into();
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+type Col = (Vec<u64>, char);
+
+fn process(input: &str) -> u64 {
+    let mut problems: HashMap<usize, Col> = HashMap::new();
+
+    input.lines().for_each(|l| {
+        for (idx, col) in l.split_whitespace().enumerate() {
+            let entry = problems.entry(idx).or_insert((vec![], '+'));
+
+            match col.parse() {
+                Ok(n) => entry.0.push(n),
+                Err(_) => match col {
+                    "+" => entry.1 = '+',
+                    "*" => entry.1 = '*',
+                    _ => panic!("ohno"),
+                },
+            }
+        }
+    });
+
+    problems
+        .into_values()
+        .map(|(nums, op)| match op {
+            '+' => nums.into_iter().sum::<u64>(),
+            '*' => nums.into_iter().product(),
+            _ => panic!("ohno"),
+        })
+        .sum()
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process(include_str!("./sample.txt")), 4277556);
+    }
+}
--- 2025/day06/src/bin/part2.rs
+++ 2025/day06/src/bin/part2.rs
@@ -1,18 +1,76 @@
+use std::collections::BTreeMap;
+
 fn main() {
     let input: String = common::AocInput::fetch(2025, 6).unwrap().into();
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+fn process(input: &str) -> usize {
+    let mut transposed: BTreeMap<usize, Vec<char>> = BTreeMap::new();
+
+    input.lines().for_each(|l| {
+        l.char_indices().for_each(|(x, c)| {
+            let e = transposed.entry(x).or_default();
+            e.push(c);
+        });
+    });
+
+    let mut problems: Vec<(Vec<usize>, char)> = vec![(vec![], '+')];
+    for col in transposed.values() {
+        if col.iter().all(|c| c.is_whitespace()) {
+            problems.push((vec![], '+'));
+            continue;
+        }
+
+        let mut digit_found = false;
+        let num = &col[..col.len()]
+            .iter()
+            .map(|c| match c.is_ascii_digit() {
+                true => {
+                    digit_found = true;
+                    c
+                }
+                false => {
+                    if digit_found {
+                        c
+                    } else {
+                        &'0'
+                    }
+                }
+            })
+            .filter(|c| c.is_ascii_digit())
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+
+        let last = *col.last().unwrap();
+        let current = problems.last_mut().unwrap();
+        match last {
+            '*' | '+' => {
+                current.1 = last;
+                current.0.push(*num);
+            }
+            _ => {
+                current.0.push(*num);
+            }
+        };
+    }
+    problems
+        .into_iter()
+        .map(|(nums, op)| match op {
+            '+' => nums.into_iter().sum::<usize>(),
+            '*' => nums.into_iter().product(),
+            _ => panic!("ohno"),
+        })
+        .sum()
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_processes() {
+        assert_eq!(process(include_str!("./sample.txt")), 3263827);
+    }
+}
