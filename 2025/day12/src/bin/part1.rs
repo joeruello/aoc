71a95d3e43a8d38f4862c55bc76582886d4a1327
Joe Ruello <joeruello@gmail.com>
2025-12-27 16:25:34 +1100

    day 12

--- 2025/day12/src/bin/part1.rs
+++ 2025/day12/src/bin/part1.rs
@@ -1,18 +1,63 @@
+use common::Itertools;
+
 fn main() {
     let input: String = common::AocInput::fetch(2025, 12).unwrap().into();
     println!("Output: {}", process(&input));
 }
 
-fn process(input: &str) -> i32 {
-    todo!();
+#[derive(Debug)]
+struct Region {
+    w: usize,
+    h: usize,
+    counts: [usize; 6],
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_processes() {
-//         assert_eq!(process(include_str!("./sample.txt")), 3);
-//     }
-// }
+impl Region {
+    pub fn area(&self) -> usize {
+        self.w * self.h
+    }
+}
+
+fn process(input: &str) -> usize {
+    let groups = input.split("\n\n").collect_vec();
+    let shapes = &groups[0..groups.len() - 1];
+
+    // packing in NP-hard.. but input is crafted so we just need to check areas :)
+    let shapes: [usize; 6] = shapes
+        .iter()
+        .map(|s| s.chars().filter(|&c| c == '#').count())
+        .collect_vec()
+        .try_into()
+        .unwrap();
+
+    let regions = groups[groups.len() - 1]
+        .lines()
+        .map(|l| {
+            let (wh, counts) = l.split_once(": ").unwrap();
+            let (w, h) = wh.split_once("x").unwrap();
+            let counts = counts
+                .splitn(6, ' ')
+                .map(|d| d.parse::<usize>().unwrap())
+                .collect_vec();
+
+            Region {
+                w: w.parse().unwrap(),
+                h: h.parse().unwrap(),
+                counts: counts.try_into().unwrap(),
+            }
+        })
+        .collect_vec();
+
+    regions
+        .into_iter()
+        .filter(|r| {
+            let needed_area = r
+                .counts
+                .iter()
+                .zip(shapes)
+                .map(|(&c, s)| c * s)
+                .sum::<usize>();
+            r.area() > needed_area
+        })
+        .count()
+}
